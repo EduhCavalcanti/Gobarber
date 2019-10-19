fafbbdff2c942ad363b08e67905ffaf7a509ad6b
eduhcavalcanti <eduh.cavalcanti@hotmail.com>
2019-10-19 19:00:19 -0300

    ajeitar o update, o resto tudo funcional

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from '../models/User';
 
+
 class userController {
   async store(req, res) {
     const exists = await User.findOne({
@@ -21,8 +22,32 @@ class userController {
 
   };
   async update(req, res) {
-    return res.json({ Ok: 'true' })
-  }
+    const { email, oldPassword } = req.body;
+    //Vai buscar o id do usuário no bando de dados
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const exists = await User.findOne({
+        where: { email }
+      });
+      if (exists) {
+        return res.status(400).json('Email já existe');
+      };
+    };
+    //Só vai cair aqui se a senha dele bate e se for fazer a troca da senha 
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ Erro: "Senha não confere" });
+    };
+    //Se passar por todo os ifs vai poder alterar 
+    const { name, provider, id } = await User.update(req.body);
+
+    return res.json({
+      email,
+      name,
+      provider,
+      id
+    });
+  };
 };
 
 export default new userController();
--- src/router.js
+++ src/router.js
@@ -2,12 +2,13 @@ import { Router } from 'express';
 
 import userController from './app/controllers/userController';
 import sessionController from './app/controllers/sessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/', userController.store);
 routes.post('/session', sessionController.store);
 
-routes.put('/update', userController.update)
+routes.put('/update', authMiddleware, userController.update)
 
 export default routes;
